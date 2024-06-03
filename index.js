9f074c51a8def146d8aa3ee9b7a9b5905c4da6ba
ajayygurjar <ajay1651@gmail.com>
2024-06-03 14:28:25 +0530

    getElementsByTagName

--- index.js
+++ index.js
@@ -19,8 +19,18 @@
 // STEP:- 2getElementsByClassName
 
 // Write your code below:
-const fruit=document.getElementsByClassName("fruit")
-fruit[2].style.backgroundColor="yellow"
-for(let i=0;i<fruit.length;i++){
-  fruit[i].style.fontWeight="bold";
+// const fruit=document.getElementsByClassName("fruit")
+// fruit[2].style.backgroundColor="yellow"
+// for(let i=0;i<fruit.length;i++){
+//   fruit[i].style.fontWeight="bold";
+// }
+
+
+//STEP:- 3 getElementsByTagName
+
+// Write your code below:
+const listName=document.getElementsByTagName("li");
+listName[4].style.color='blue'
+for(let i=0;i<listName.length;i++){
+  listName[i].style.fontStyle='italic';
 }
